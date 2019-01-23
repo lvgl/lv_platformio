e87eced05b9645c04b3f112b7afafc08c16c27b7
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-01-23 14:10:25 +0100

    add pio project

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# pc_simulator_sdl_platformio
-SDL drivers to run LittlevGL on PC in Platformio 
+# Run LittelvGL in on PC using PlatfomIO
 
-.pioenvs/native/program
+This repository contains a PlatformIO project to test LittelvGL on your PC. 
+
+### Install SDL
+
+The [SDL library](https://www.libsdl.org/) need to installed to run the project. You will find a step-by-step GUI about the installation [here](https://docs.littlevgl.com/#PC-simulator)
+(It's super easy on Linux but sometimes tricky on Windows and OSX.)
+
+### Open the project
+I assume that you already have a working PlatformIO environment with Visual Studio Code.
+1. Clone this repo with `git clone https://github.com/littlevgl/pc_simulator_sdl_platformio.git` anywhere you want or [download it as zip from GitHub](https://github.com/littlevgl/pc_simulator_sdl_platformio/archive/master.zip).
+2. In Visual Studio Code click **File/Add folder to workspace** and browse the **lv_pio_sdl** folder in the cloned or downloaded project
+
+## Build and run
+3. Click the pipe icon on the left bottom corner to build the project. 
+4. Run the compiled file from the terminal of Visual Studio Code: `./.pioenvs/native/program`
+
+
+
+
---
+++ lv_pio_sdl/src/main.c
@@ -0,0 +1,76 @@
+
+#include <stdlib.h>
+#include <unistd.h>
+#define SDL_MAIN_HANDLED        /*To fix SDL's "undefined reference to WinMain" issue*/
+#include <SDL2/SDL.h>
+#include <lvgl.h>
+#include "monitor.h"
+#include "mouse.h"
+#include "mousewheel.h"
+#include "keyboard.h"
+#include "demo.h"
+
+
+static void hal_init(void);
+static int tick_thread(void * data);
+int main(void)
+{
+
+    printf("hello\n");
+    lv_init();
+    hal_init();
+
+    demo_create();
+
+    while (1) {
+        lv_task_handler();
+        usleep(10* 1000);
+    }
+}
+
+/**
+ * Initialize the Hardware Abstraction Layer (HAL) for the Littlev graphics library
+ */
+static void hal_init(void)
+{
+    /* Add a display
+     * Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
+    monitor_init();
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
+    disp_drv.disp_flush = monitor_flush;    /*Used when `LV_VDB_SIZE != 0` in lv_conf.h (buffered drawing)*/
+    disp_drv.disp_fill = monitor_fill;      /*Used when `LV_VDB_SIZE == 0` in lv_conf.h (unbuffered drawing)*/
+    disp_drv.disp_map = monitor_map;        /*Used when `LV_VDB_SIZE == 0` in lv_conf.h (unbuffered drawing)*/
+    lv_disp_drv_register(&disp_drv);
+
+    /* Add the mouse as input device
+     * Use the 'mouse' driver which reads the PC's mouse*/
+    mouse_init();
+    lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);          /*Basic initialization*/
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read = mouse_read;         /*This function will be called periodically (by the library) to get the mouse position and state*/
+    lv_indev_drv_register(&indev_drv);
+
+    /* Tick init.
+     * You have to call 'lv_tick_inc()' in periodically to inform LittelvGL about how much time were elapsed
+     * Create an SDL thread to do this*/
+    SDL_CreateThread(tick_thread, "tick", NULL);
+}
+
+/**
+ * A task to measure the elapsed time for LittlevGL
+ * @param data unused
+ * @return never return
+ */
+static int tick_thread(void * data)
+{
+    (void)data;
+
+    while(1) {
+        SDL_Delay(5);   /*Sleep for 5 millisecond*/
+        lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
+    }
+
+    return 0;
+}
