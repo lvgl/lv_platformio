ff3e75391d7332ca7375ab965168dfd0749a5547
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-02-14 18:20:19 +0100

    add stm files

--- lv_pio_sdl/src/main.c
+++ lv_pio_sdl/src/main.c
@@ -1,43 +1,158 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
 
-#include <stdlib.h>
 
-#include <lvgl.h>
+#include "lvgl/lvgl.h"
 
 #if BUILD_ENV_NAME == native
 #include <unistd.h>
 #define SDL_MAIN_HANDLED        /*To fix SDL's "undefined reference to WinMain" issue*/
 #include <SDL2/SDL.h>
-#include "monitor.h"
-#include "mouse.h"
-#include "mousewheel.h"
-#include "keyboard.h"
+#include "sdl/monitor.h"
+#include "sdl/mouse.h"
+#include "sdl/mousewheel.h"
+#include "sdl/keyboard.h"
+#endif
+
+
+#if BUILD_ENV_NAME == stm32f429_disco
+#include "stm32f429_disco/stm32f429i_discovery.h"
+#include "stm32f429_disco/tft/tft.h"
+#include "stm32f429_disco/touchpad/touchpad.h"
 #endif
 
 #include "demo.h"
 
+static void SystemClock_Config(void);
 
-static void hal_init(void);
-static int tick_thread(void * data);
 int main(void)
 {
-    printf("Start\n");
-    lv_init();
-    hal_init();
+	lv_init();
 
-    demo_create();
+#if BUILD_ENV_NAME == native
+  hal_init();
+#endif
 
-    while (1) {
-        lv_task_handler();
-        usleep(10* 1000);
-    }
+#if BUILD_ENV_NAME == stm32f429_disco
+	HAL_Init();
+
+	/* Configure the system clock to 180 MHz */
+	SystemClock_Config();
+
+	/*Start up indication*/
+	BSP_LED_Init(LED3);
+	uint8_t i;
+	for (i = 0; i < 8; i++) {
+		BSP_LED_Toggle(LED3);
+		HAL_Delay(50);
+	}
+
+	tft_init();
+	touchpad_init();
+#endif
+
+
+	demo_create();
+
+	while (1)
+	{
+		HAL_Delay(5);
+		lv_task_handler();
+	}
 }
 
+
+#if BUILD_ENV_NAME == stm32f429_disco
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 180000000
+  *            HCLK(Hz)                       = 180000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 360
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  *         The LTDC Clock is configured as follow :
+  *            PLLSAIN                        = 192
+  *            PLLSAIR                        = 4
+  *            PLLSAIDivR                     = 8
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the device is
+     clocked below the maximum system frequency, to update the voltage scaling value
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /*##-1- System Clock Configuration #########################################*/
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Activate the Over-Drive mode */
+  HAL_PWREx_EnableOverDrive();
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+
+  /*##-2- LTDC Clock Configuration ###########################################*/
+  /* LCD clock configuration */
+  /* PLLSAI_VCO Input = HSE_VALUE/PLL_M = 1 MHz */
+  /* PLLSAI_VCO Output = PLLSAI_VCO Input * PLLSAIN = 192 MHz */
+  /* PLLLCDCLK = PLLSAI_VCO Output/PLLSAIR = 192/4 = 48 MHz */
+  /* LTDC clock frequency = PLLLCDCLK / RCC_PLLSAIDIVR_8 = 48/8 = 6 MHz */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
+  PeriphClkInitStruct.PLLSAI.PLLSAIR = 4;
+  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_8;
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+}
+#endif
+
+
+#if BUILD_ENV_NAME == native
 /**
  * Initialize the Hardware Abstraction Layer (HAL) for the Littlev graphics library
  */
 static void hal_init(void)
 {
-#if BUILD_ENV_NAME == native
     /* Add a display
      * Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
     monitor_init();
@@ -61,10 +176,6 @@ static void hal_init(void)
      * You have to call 'lv_tick_inc()' in periodically to inform LittelvGL about how much time were elapsed
      * Create an SDL thread to do this*/
     SDL_CreateThread(tick_thread, "tick", NULL);
-#elif BUILD_ENV_NAME == stm32
-
-
-#endif
 }
 
 /**
@@ -83,3 +194,5 @@ static int tick_thread(void * data)
 
     return 0;
 }
+
+#endif
---
+++ lv_pio_sdl/src/stm32f4xx_it.c
@@ -0,0 +1,47 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_it.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    02-Feb-2015
+  * @brief   Default Interrupt Service Routines.
+  ******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx.h"
+#ifdef USE_RTOS_SYSTICK
+#include <cmsis_os.h>
+#endif
+#include "stm32f4xx_it.h"
+
+#include "lvgl/lv_hal/lv_hal_tick.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            	  	    Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles SysTick Handler, but only if no RTOS defines it.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+
+	lv_tick_inc(1);
+
+#ifdef USE_RTOS_SYSTICK
+	osSystickHandler();
+#endif
+}
