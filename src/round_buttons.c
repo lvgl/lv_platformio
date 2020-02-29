45c9c2b1cdb14dac07fe59dc2993d6e33f07bc1d
yhfudev <yhfudev@gmail.com>
2020-02-29 12:06:26 -0500

    adjust the button positions by screen size.

--- src/round_buttons.c
+++ src/round_buttons.c
@@ -13,7 +13,11 @@ void round_buttons(void)
 	lv_obj_t * circle,*btn,*label,*img;
 	static lv_style_t circle_style,rel_style,prs_style;
 	circle=lv_obj_create(lv_scr_act(),NULL);
-	lv_obj_set_size(circle, 240, 240);
+	int r = TFT_WIDTH;
+	if (r > TFT_HEIGHT) r = TFT_HEIGHT;
+	r = r / 2;
+	lv_obj_set_size(circle, r*2, r*2);
+	int x = (1 - 0.70710678) * r; // sin(pi/4) = 0.70710678
 
 	lv_style_copy(&circle_style,&lv_style_pretty_color);
 	lv_style_btn_rel.body.radius=LV_RADIUS_CIRCLE;
@@ -21,7 +25,6 @@ void round_buttons(void)
 	circle_style.body.radius=LV_RADIUS_CIRCLE;
 	lv_obj_set_style(circle, &circle_style);
 	lv_obj_align(circle, NULL, LV_ALIGN_CENTER, 0, 0);
-
 
 	btn=lv_btn_create(circle, NULL);
 	lv_obj_set_size(btn, 60, 60);
@@ -39,7 +42,7 @@ void round_buttons(void)
 	rotary_group_add(btn);
 	lv_btn_set_style(btn, LV_BTN_STATE_REL, &lv_style_btn_rel);
 	lv_btn_set_style(btn, LV_BTN_STATE_PR, &lv_style_btn_pr);
-	lv_obj_align(btn, circle, LV_ALIGN_IN_TOP_RIGHT, -30,30);
+	lv_obj_align(btn, circle, LV_ALIGN_IN_TOP_RIGHT, -x,x);
 	img = lv_img_create(btn, NULL);
 	lv_img_set_auto_size(img, true);
 	lv_obj_set_click(img, false);
@@ -61,7 +64,7 @@ void round_buttons(void)
 	rotary_group_add(btn);
 	lv_btn_set_style(btn, LV_BTN_STATE_REL, &lv_style_btn_rel);
 	lv_btn_set_style(btn, LV_BTN_STATE_PR, &lv_style_btn_pr);
-	lv_obj_align(btn, circle, LV_ALIGN_IN_BOTTOM_RIGHT, -30,-30);
+	lv_obj_align(btn, circle, LV_ALIGN_IN_BOTTOM_RIGHT, -x,-x);
 	img = lv_img_create(btn, NULL);
 	lv_img_set_auto_size(img, true);
 	lv_obj_set_click(img, false);
@@ -83,7 +86,7 @@ void round_buttons(void)
 	rotary_group_add(btn);
 	lv_btn_set_style(btn, LV_BTN_STATE_REL, &lv_style_btn_rel);
 	lv_btn_set_style(btn, LV_BTN_STATE_PR, &lv_style_btn_pr);
-	lv_obj_align(btn, circle, LV_ALIGN_IN_BOTTOM_LEFT, 30,-30);
+	lv_obj_align(btn, circle, LV_ALIGN_IN_BOTTOM_LEFT, x,-x);
 	img = lv_img_create(btn, NULL);
 	lv_img_set_auto_size(img, true);
 	lv_obj_set_click(img, false);
@@ -105,7 +108,7 @@ void round_buttons(void)
 	rotary_group_add(btn);
 	lv_btn_set_style(btn, LV_BTN_STATE_REL, &lv_style_btn_rel);
 	lv_btn_set_style(btn, LV_BTN_STATE_PR, &lv_style_btn_pr);
-	lv_obj_align(btn, circle, LV_ALIGN_IN_TOP_LEFT, 30,30);
+	lv_obj_align(btn, circle, LV_ALIGN_IN_TOP_LEFT, x,x);
 	img = lv_img_create(btn, NULL);
 	lv_img_set_auto_size(img, true);
 	lv_obj_set_click(img, false);
