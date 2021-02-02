4f1dc9a324c9c9e538ea659bb4de8b243d7c5189
Vitaly Puzrin <vitaly@rcdesign.ru>
2021-02-03 01:10:12 +0300

    Move "execute" task to targets, close #30

--- support/sdl2_build_extra.py
+++ support/sdl2_build_extra.py
@@ -6,5 +6,19 @@ for e in [ env, projenv ]:
     if "-m32" in e['CCFLAGS']:
         e.Append(LINKFLAGS = ["-m32"])
 
+# Override unused "upload" to execute compiled binary
+#from SCons.Script import AlwaysBuild
+#AlwaysBuild(env.Alias("upload", "$BUILD_DIR/${PROGNAME}", "$BUILD_DIR/${PROGNAME}"))
+
+# Add custom target to explorer
+env.AddTarget(
+    name = "execute",
+    dependencies = "$BUILD_DIR/${PROGNAME}",
+    actions = "$BUILD_DIR/${PROGNAME}",
+    title = "Execute",
+    description = "Build and execute",
+    group="General"
+)
+
 #print('=====================================')
 #print(env.Dump())
